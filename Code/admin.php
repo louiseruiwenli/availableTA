0dc38a286c6163f8a508f7b0a779cf6b64afbecb
Ruiwen <louiseruiwenli@gmail.com>
2017-10-26 22:36:49 -0700

    admin page

--- Code/admin.php
+++ Code/admin.php
@@ -8,15 +8,46 @@
 </head>
 
 <body>
+
     <div id="wrap">
+
         <div class="container">
+            <div class="row">
+                <h1 class="text-center">AvailableTA Admin</h1>
+            </div>
             <div class="row">
 
                 <form class="form-horizontal" action="functions.php" method="post" name="upload_excel" enctype="multipart/form-data">
                     <fieldset>
-
                         <!-- Form Name -->
-                        <legend>Form Name</legend>
+                        <legend>Upload TA and Professor Information</legend>
+
+                        <!-- File Button -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="filebutton">Select File</label>
+                            <div class="col-md-4">
+                                <input type="file" name="file" id="file" class="input-large">
+                            </div>
+                        </div>
+
+                        <!-- Button -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="singlebutton">Import data</label>
+                            <div class="col-md-4">
+                                <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading" data-loading-text="Loading...">Import</button>
+                            </div>
+                        </div>
+
+                    </fieldset>
+                </form>
+
+            </div>
+            <div class="row">
+
+                <form class="form-horizontal" action="functions.php" method="post" name="upload_excel" enctype="multipart/form-data">
+                    <fieldset>
+                        <!-- Form Name -->
+                        <legend>Upload Lab Session Information</legend>
 
                         <!-- File Button -->
                         <div class="form-group">
