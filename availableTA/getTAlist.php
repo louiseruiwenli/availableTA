9c9ea51fbefb20e01f85c444e8e4e99cc18f4e6f
Ruiwen <louiseruiwenli@gmail.com>
2017-11-23 23:10:09 -0800

    display warning message

--- availableTA/getTAlist.php
+++ availableTA/getTAlist.php
@@ -30,20 +30,21 @@ $result_TAlist=mysqli_query($link, $sql_TAlist) or die($result_TAlist);
 if(isset($result_TAlist)){
   if(mysqli_num_rows($result_TAlist)===0){
     echo json_encode("No Result");
-  }
-  $rows = array();
-  while($row = mysqli_fetch_array($result_TAlist,MYSQLI_NUM)){
-    $taID = $row[0];
-    $sql_user = "SELECT Name, email, phone FROM User WHERE ID = '$taID' AND TAProf = '0'";
-    $result_user = mysqli_query($link, $sql_user);
-    if(isset($result_user)){
-      while($row= mysqli_fetch_assoc($result_user)){
-        $rows[] = $row;
+  }else{
+    $rows = array();
+    while($row = mysqli_fetch_array($result_TAlist,MYSQLI_NUM)){
+      $taID = $row[0];
+      $sql_user = "SELECT Name, email, phone FROM User WHERE ID = '$taID' AND TAProf = '0'";
+      $result_user = mysqli_query($link, $sql_user);
+      if(isset($result_user)){
+        while($row= mysqli_fetch_assoc($result_user)){
+          $rows[] = $row;
+        }
       }
     }
+    echo json_encode($rows);
   }
 
-  echo json_encode($rows);
 }else{
    echo json_encode("No Result");
 }
--- availableTA/taList.js
+++ availableTA/taList.js
@@ -13,18 +13,25 @@ $(document).ready(function(){
 
   $.get('getTAlist.php?labID='+labID).done(function(result){
       var result_json = $.parseJSON(result);
-      $.each(result_json, function(index, value){
-          var div = document.createElement('div');
-          $(div).addClass('col-md-6')
-                .addClass('TAinfo')
-                .appendTo($('#talist'));
-          var name = document.createElement('h4');
-          $(name).html(value['Name']).appendTo($(div));
-          var email = document.createElement('p');
-          $(email).html('Email: ' + value['email']).appendTo($(div));
-          var phone = document.createElement('p');
-          $(phone).html('Phone: ' + value['phone']).appendTo($(div));
-      });
+      if(result_json=='No Result'){
+        var warning = document.createElement('h4');
+        $(h4).html('Warning: No TA available during this time period. Please find another solution or contact Professor Atkinson at datkinson@scu.edu')
+             .appendTo($('#talist'));
+      }else{
+        $.each(result_json, function(index, value){
+            var div = document.createElement('div');
+            $(div).addClass('col-md-6')
+                  .addClass('TAinfo')
+                  .appendTo($('#talist'));
+            var name = document.createElement('h4');
+            $(name).html(value['Name']).appendTo($(div));
+            var email = document.createElement('p');
+            $(email).html('Email: ' + value['email']).appendTo($(div));
+            var phone = document.createElement('p');
+            $(phone).html('Phone: ' + value['phone']).appendTo($(div));
+        });
+      }
+
 
   });
 
